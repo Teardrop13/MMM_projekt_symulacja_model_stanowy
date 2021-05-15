494638d45d3357b9374cf5163ae69b3f9703fc44
Teardrop13 <michal.lewandowski99@gmail.com>
2021-05-15 20:51:07 +0200

    odczyt parametrow z pol tekstowych

--- simulator.py
+++ simulator.py
@@ -7,71 +7,86 @@ import sys
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 
-n = 3 # parametr zadany, wielkosc macierzy
+def createInputVector(type, time, dt, period, amplitude):
+    # tworzenie wektora sygnalu zadanego
+    # type podany jako string: 'sinus' 'square' 'triangle'
+    step_count = int(time/dt)
+    u = []
 
-t = 50 # czas symulacji
-dt = 0.001 # czas kroku
-step_count = int(t/dt) # liczba krokow
+    return u
 
-A = [[-5, 1, 0], [-2, 0, 1], [-1, 0, 0]]
-B = [1, 1, 2] # czy jest jakas zaleznosc ze na ostatniej pozycji musi byc 1 ?
+def createTimeVector(time, dt):
+    step_count = int(time/dt)
+    t = []
+    for i in range(step_count):
+        t.append(i*dt)
+    return t
 
-x = [0, 0, 0] # zmienne stanu
-y = [] # wektor wyjściowy
-t = [] # os czasu
-u = [] # wektor wejsciowy
+def createMatrixA(parameters, n):
+    A = []
+    for i in range(n):
+        row = []
+        for j in range(n):
+            row.append(0)
+        A.append(row)
+    a=0
+    b=1
+    m=n-1
+    while m:
+        A[a][b]=1
+        b=b+1
+        a=a+1
+        m=m-1
+    for i, param in enumerate(parameters):
+        A[i][0] = -param
+    return A
 
-u_integral = 0 # zerowe warunki początkowe
-
-# całka wektora zmiennych stanu
-x_integral = x.copy()
-
-# tworzenie wektora sygnalu zadanego
-for i in range(step_count):
-
-    # if i*dt > 3 and i*dt < 8:
-    #     u.append(1)
-    # else:
-    #     u.append(0)
-
-    # if i*dt > 3 and i*dt < 7:
-    #     u.append(1)
-    # elif i*dt > 8 and i*dt < 25:
-    #     u.append(2)
-    # else:
-    #     u.append(0)
-
-    u.append(1)
-
-    # u.append(math.sin(i*dt))
-
-    # u.append(5*i*dt)
-
-
-for current_step in range(0, step_count):
-    t.append(current_step*dt)
-
-    # całka z u
-    u_integral += u[current_step]*dt
-
-    # całka wektora stanu x
-    for row in range(n):
-        x_integral[row] += x[row]*dt
-
-    # tworzenie nowego wektora stanu
-    # realizacja dzialania A*x+B*u
-    for row in range(n):
-        x[row] = 0
-        for col in range(n):
-            x[row] +=  A[row][col] * x_integral[col]
-        x[row] += B[row] * u_integral
-
-    y.append(x[0]) # C = [1 0 0 0 ... ] wiec tylko zmienna stanu x0
+def createMatrixB(parameters, n):
+    B = []
+    for i in range(n):
+        col = []
+        col.append(0)
+        B.append(col)
+    for i, param in enumerate(parameters):
+        B[i][0] = param
+    return B
 
 
-# plt.plot(t, u, 'g')
-# plt.plot(t, y, 'r')
-# plt.show()
+class Model():
+    def __init__(self, A, B, n):
+        self.A = A
+        self.B = B
+        self.n = n
+
+    def simulation(self, u, t, dt):
+        u_integral = 0  # zerowe warunki początkowe
+        
+        # wektor zmiennych stanu
+        x = []
+        for i in range(self.n):
+            x.append(0)
+        x_integral = x.copy()
+
+        y = []
+
+        for current_step in range(len(t)):
+            # całka z u
+            u_integral += u[current_step]*dt
+
+            # całka wektora stanu x
+            for row in range(self.n):
+                x_integral[row] += x[row]*dt
+
+            # tworzenie nowego wektora stanu
+            # realizacja dzialania A*x+B*u
+            for row in range(self.n):
+                x[row] = 0
+                for col in range(self.n):
+                    x[row] += self.A[row][col] * x_integral[col]
+                x[row] += self.B[row][0] * u_integral
+
+            y.append(x[0])  # C = [1 0 0 0 ... ] wiec tylko zmienna stanu x0
+        return y
 
 class Simulator(QtWidgets.QMainWindow, simulator_gui.Ui_Dialog):
     def __init__(self, parent=None):
@@ -85,11 +100,91 @@ class Simulator(QtWidgets.QMainWindow, simulator_gui.Ui_Dialog):
         plot_box.addWidget(self.canvas)
         self.plotArea.setLayout(plot_box)
 
-        # rysowanie czegokolwiek w plotArea
+        # wywolywanie funkcji po wcisnieciu przycisku
+        self.runButton.clicked.connect(self.runSimulation)
+
+    def readA_parameters(self, n):
+        A_parameters = []
+        parameters_string = self.textEdit_matrixA.toPlainText()
+        parameters_list = parameters_string.split('\n')
+        for param in parameters_list:
+            if param == '':
+                break
+            A_parameters.append(float(param))
+        assert len(A_parameters) == n
+        return A_parameters
+
+    def readB_parameters(self, n):
+        B_parameters = []
+        parameters_string = self.textEdit_matrixB.toPlainText()
+        parameters_list = parameters_string.split('\n')
+        for param in parameters_list:
+            if param == '':
+                break
+            B_parameters.append(float(param))
+        assert len(B_parameters) == n
+        return B_parameters
+
+    def printMessage(self, text):
+        self.errorDisplay.setPlainText(text)
+        self.repaint()
+
+    def runSimulation(self):
+        self.printMessage('symulacja w toku')
+        n = self.parameter_n.value()  # wielkosc macierzy
+        time = self.doubleSpinBox_stopTime.value()  # czas symulacji
+        dt = self.doubleSpinBox_step.value()  # czas kroku
+        period = self.doubleSpinBox_period.value() # okres sygnalu wejsciowego
+        amplitude = self.doubleSpinBox_amplitude.value() # amplituda sygnalu wejsciowego
+        
+        signal_type = ''
+        if self.radioButton_sinus.isChecked():
+            signal_type = 'sinus'
+        elif self.radioButton_square.isChecked():
+            signal_type = 'square'
+        elif self.radioButton_triangle.isChecked():
+            signal_type = 'triangle'
+
+
+        # do usuniecia:
+        step_count = int(time/dt)
+        u = []
+        for i in range(step_count):
+            u.append(1)
+        # do usuniecia
+
+
+        try:
+            A_parameters = self.readA_parameters(n)
+        except ValueError:
+            self.printMessage('nie wlasciwy element w macierzy A')
+            return
+        except AssertionError:
+            self.printMessage('nie wlasciwa ilosc parametrów macierzy A')
+            return
+
+        try:
+            B_parameters = self.readB_parameters(n)
+        except ValueError:
+            self.printMessage('nie wlasciwy element w macierzy B')
+            return
+        except AssertionError:
+            self.printMessage('nie wlasciwa ilosc parametrów macierzy B')
+            return
+
+        A = createMatrixA(A_parameters, n)
+        B = createMatrixB(B_parameters, n)
+        #u = createInputVector(signal_type, time, dt, period, amplitude)
+        t = createTimeVector(time, dt)
+        model = Model(A, B, n)
+        y = model.simulation(u, t, dt)
+        self.printMessage('symulacja gotowa')
+
+        # rysowanie wykresu
         self.figure.clear()
-        ax = self.figure.add_subplot()
-        ax.plot(t, u, 'g')
-        ax.plot(t, y, 'r')
+        self.ax = self.figure.add_subplot()
+        self.ax.plot(t, u, 'g')
+        self.ax.plot(t, y, 'r')
         self.canvas.draw()
 
 
