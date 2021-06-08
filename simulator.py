dad4b3f9ba2d87846f2813ddbf1fdc72f3d00bb2
Teardrop13 <michal.lewandowski99@gmail.com>
2021-06-08 17:15:01 +0200

    tworzenie wektora wejściowego

--- simulator.py
+++ simulator.py
@@ -10,13 +10,37 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 def createInputVector(type, time, dt, period, amplitude):
     # tworzenie wektora sygnalu zadanego
     # type podany jako string: 'sinus' 'square' 'triangle'
-    step_count = int(time/dt)
+    step_count = int(time/dt) + 1
     u = []
+    k=0
+
+    if (type=='sinus'):
+        for i in range(step_count):
+            u.append(amplitude*math.sin(2*math.pi/period*i*dt))
+    elif (type=='square'):
+        for i in range(step_count):
+            if (math.sin(2*math.pi/period*i*dt) > 0):
+                k = 1
+            elif (math.sin(2*math.pi/period*i*dt) <= 0):
+                k = -1
+            u.append(amplitude * k)
+    elif (type=='triangle'):
+        for i in range(step_count):
+            k=2*amplitude/math.pi*math.asin(math.sin(2*math.pi*i*dt/period))
+            u.append(k)
+    elif (type=='ramp'):
+        for i in range(step_count):
+            period1=period/math.pi
+            k=2*amplitude/math.pi*math.atan(math.tan(i*dt/period1))
+            u.append(k)
+    elif (type=='step'):
+        for i in range(step_count):
+            u.append(amplitude)
 
     return u
 
 def createTimeVector(time, dt):
-    step_count = int(time/dt)
+    step_count = int(time/dt) + 1
     t = []
     for i in range(step_count):
         t.append(i*dt)
@@ -144,15 +168,10 @@ class Simulator(QtWidgets.QMainWindow, simulator_gui.Ui_Dialog):
             signal_type = 'square'
         elif self.radioButton_triangle.isChecked():
             signal_type = 'triangle'
-
-
-        # do usuniecia:
-        step_count = int(time/dt)
-        u = []
-        for i in range(step_count):
-            u.append(1)
-        # do usuniecia
-
+        elif self.radioButton_ramp.isChecked():
+            signal_type = 'ramp'
+        elif self.radioButton_step.isChecked():
+            signal_type = 'step'
 
         try:
             A_parameters = self.readA_parameters(n)
@@ -174,11 +193,15 @@ class Simulator(QtWidgets.QMainWindow, simulator_gui.Ui_Dialog):
 
         A = createMatrixA(A_parameters, n)
         B = createMatrixB(B_parameters, n)
-        #u = createInputVector(signal_type, time, dt, period, amplitude)
+        u = createInputVector(signal_type, time, dt, period, amplitude)
         t = createTimeVector(time, dt)
         model = Model(A, B, n)
         y = model.simulation(u, t, dt)
         self.printMessage('symulacja gotowa')
+
+        print(u)
+        print(y)
+        print(t)
 
         # rysowanie wykresu
         self.figure.clear()
